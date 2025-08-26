87743910c8972d618302341905d981ee895b2db8
Hungb <giahung092007@gmail.com>
2025-08-26 20:16:13 +0300

    Add homework Question1.py Question2.py Question3.py Question4.py

--- Module 3/Question4.py
+++ Module 3/Question4.py
@@ -1,7 +1,9 @@
 year = int(input("Enter a year: "))
-if year % 4 == 0 and not year % 100 == 0:
+if year % 400 == 0:
     print(f"{year} is a leap year.")
-elif year % 400 == 0:
+elif year % 100 == 0:
+    print(f"{year} is not a leap year.")
+elif year % 4 ==0:
     print(f"{year} is a leap year.")
 else:
     print(f"{year} is not a leap year.")
