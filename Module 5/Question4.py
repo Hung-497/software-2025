29cdd956d92f1aeae20fb506e5983682ec3615a0
Hung-497 <giahung092007@gmail.com>
2025-09-03 14:57:03 +0300

    Add homework

---
+++ Module 5/Question4.py
@@ -0,0 +1,7 @@
+city = input("Enter the name of a city: ")
+cities = []
+for n in cities:
+    while city != "":
+        cities.append(city)
+        city = input("Enter the name of a city: ")
+    print(f'The city you entered: {n}')
---
+++ Module 6/Question1.py
@@ -0,0 +1,6 @@
+def sum_of_list(integers):
+    sumOfNumbers = sum(numbers)
+    return sumOfNumbers
+numbers = [1,2,3]
+sum = sum_of_list(numbers)
+print(f'Sum of the numbers was: {sum}')
