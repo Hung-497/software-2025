d3660d4e19033524e7af35239d9ab0bf2bfea230
Hung-497 <giahung092007@gmail.com>
2025-09-04 10:17:42 +0300

    Add homework

--- Module 5/Question4.py
+++ Module 5/Question4.py
@@ -1,13 +1,10 @@
 cities = []
-# while city != "":
-#     cities.append(city)
-#     city = input("Enter the name of a city: ")
 
 for i in range(5):
     city = input(f"Enter the name of a city: ")
     i +=1
     cities.append(city)
-
+print("")
 print("\nThe cities you entered: ")
 for n in cities:
     print(n)
