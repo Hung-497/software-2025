8a6b35ca663a1ee7a6cdb2b08e9392ecdcba04b1
Hung-497 <giahung092007@gmail.com>
2025-09-07 15:37:23 +0300

    Add homework

--- Module 6/Question5.py
+++ Module 6/Question5.py
@@ -1,5 +1,11 @@
 def filter_even_numbers(numbers):
-
+    n = []
+    for i in numbers:
+        if i % 2 == 0 :
+            n.append(i)
+    return n
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even = filter_even_numbers(numbers)
 print(f"Original list: {numbers}")
+print(f'List with even numbers only: {even}')
