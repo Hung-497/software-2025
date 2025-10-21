d6f46a5e9b40cea0262b45a6452d11dc46d7cd00
Hung-497 <giahung092007@gmail.com>
2025-10-21 13:11:39 +0300

    Project

--- Software2/Module 10/Assignment4.py
+++ Software2/Module 10/Assignment4.py
@@ -27,9 +27,31 @@ class Race:
             car.accelerate(random.randint(-10,15))
             car.drive(1)
     def print_status(self):
-        print(f"{'Plate'} {'Max(km/h)'} {'Speed(km/h)'} {'Distance(km)'}")
-        print("-" * 50)
+        print("Plate | Max Speed(km/h) | Current Speed(km/h) | Distance(km)")
+        print("-" * 60)
         for car in self.cars:
-            print(f"{car.license_plate} {car.maximum_speed} {car.current_speed} {car.travelled_distance}")
+            print(f"{car.license_plate:6}|{car.maximum_speed:17}|{car.current_speed:21}|{car.travelled_distance:12.1f}")
     def race_finished(self):
         return any(car.travelled_distance >= self.distance for car in self.cars)
+
+cars = []
+for i in range(1, 11):
+    license_plate = f"ABC-{i}"
+    maximum_speed = random.randint(100, 200)
+    car = Car(license_plate, maximum_speed)
+    cars.append(car)
+
+race = Race("Grand Demolition Derby", 8000, cars)
+
+hours = 0
+while not race.race_finished():
+    race.hour_passes()
+    hours += 1
+
+    if hours % 10 == 0:
+        print(f"\nStatus after {hours} hours:")
+        race.print_status()
+
+print(f"\nRace finished after {hours} hours!")
+print("Final results:")
+race.print_status()
